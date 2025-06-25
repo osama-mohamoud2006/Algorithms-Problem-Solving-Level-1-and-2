e87c9d2791d57d9c7bdb1a56a4592c12f81393b0
osama mohamoud <wwwhdosama@gmail.com>
2025-06-25 20:29:59 +0300

    Auto commit: 2025-06-25 20:29:59

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,36 +1,28 @@
-#include <iostream>   
-#include <string>    
-using namespace std;  
-
-
-int ReadPositiveNumber(string message)
-{
-    int Number = 0; // Initialize a variable to store the user's input.
-    do
-    {
-        cout << message << endl; // Display the prompt message.
-        cin >> Number;           // Read the user's input.
-    } while (Number <= 0);       // Continue looping until a positive number is entered.
-
-    return Number;             // Return the validated positive number.
+#include <iostream>
+using namespace std;
+int input(string text) {
+	int number  = 0;
+	do {
+		cout << text << " ";
+		cin >> number;
+	} while (number <= 0);
+	return number;
 }
+bool check(int it) {
+	int sum = 0; 
 
-// perfect square is all numbers that can be divisable by number and the sum of them  are equal to number 
-bool  check(int n) {
-    int sum = 0;
-    for (int i = 1; i < n; i++) {
-        // 6
-        //10 
-        if (n % i == 0)  sum  += i ;
-    }
-    return sum == n;
+	for (int i = 1; i < it; i++) {
+		/// check perfect number
+		if (it % i == 0) sum += i;
+	}
+	return sum == it;
 }
-void print(int n) {
-    cout << "perfect nums are : " << endl;
-    for (int i = 1; i < n; i++) {
-        if (check(i)) cout << i << endl;
-    }
+void print(int number) {
+	cout << "the perfect square are: " << endl;
+	for (int i = 1; i < number; i++) {
+		if (check(i)) cout << i << endl;
+	}
 }
 int main() {
-    print(ReadPositiveNumber("enter: "));
+	print(input("enter number:"));
 }
