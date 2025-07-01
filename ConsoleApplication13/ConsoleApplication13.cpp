32b29e76b79ed37dbce45c1ab009615269c8b2fc
osama mohamoud <wwwhdosama@gmail.com>
2025-07-01 19:05:43 +0300

    Auto commit: 2025-07-01 19:05:43

---
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int input(string message) {
+	int n;
+	do {
+		cout << message;
+		cin >> n;
+	} while (0 > n);
+	return n;
+ }
+
+void pattern(int n) {
+	for (int i = 1; i <= n; i++) { // for lines
+		// loop to repeat thing at specified line 
+		for (int j = i; j >0; j--) {
+			cout << i;
+		}
+		cout << endl;
+	}
+
+}
+
+int main() {
+	int n = input("enter postive number: ");
+	pattern(n);
+}
