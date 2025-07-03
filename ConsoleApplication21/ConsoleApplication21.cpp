5c04b94e45022fc5d3e80f80a5e7f2e334b510d5
osama mohamoud <wwwhdosama@gmail.com>
2025-07-03 21:48:50 +0300

    Auto commit: 2025-07-03 21:48:49

--- ConsoleApplication21/ConsoleApplication21.cpp
+++ ConsoleApplication21/ConsoleApplication21.cpp
@@ -5,7 +5,8 @@ using namespace std;
 // first ask user to input how many keys he wanna
 // second make random function
 // third make for loop from 1 to inputed value and generate n lines of random things
-
+enum enCharType { SamallLetter = 1, CapitalLetter = 2, SpecialCharacter = 3, Digit = 4 };
+// input the num of keys 
 int input() {
 	int n;
 	do {
@@ -15,29 +16,75 @@ int input() {
 	} while (1 > n);
 	return n;
 }
-
-int random(int from, int to) {
+// generate random value
+int RandomNumber(int from, int to) {
 	int random_shit = rand() % (to - from + 1) + from;
 	return random_shit;
 
 }
-string random_generator() {
+// get random things based on enum input 
+char GetRandomCharacter(enCharType CharType)
+{
+    switch (CharType)
+    {
+    case enCharType::SamallLetter:
+    {
+        // Generate a random lowercase letter (ASCII: 97 'a' to 122 'z').
+        return char(RandomNumber(97, 122));
+        break; // break is not necessary after return.
+    }
+    case enCharType::CapitalLetter:
+    {
+        // Generate a random uppercase letter (ASCII: 65 'A' to 90 'Z').
+        return char(RandomNumber(65, 90));
+        break;
+    }
+    case enCharType::SpecialCharacter:
+    {
+        // Generate a random special character (ASCII: 33 '!' to 47 '/').
+        return char(RandomNumber(33, 47));
+        break;
+    }
+    case enCharType::Digit:
+    {
+        // Generate a random digit (ASCII: 48 '0' to 57 '9').
+        return char(RandomNumber(48, 57));
+        break;
+    }
+    }
+    // If no valid type is provided, return a null character.
+    return '\0';
+}
+
+// call random geneatror to make word 
+string random_generator(enCharType type) {
+    //geneate one word with 4 letters 
 	string res = "";
 	for (int r = 1; r <= 4; r++) {
-		res += char(random(65, 90)); //WXCY
+		res += GetRandomCharacter(type); //WXCY
 
 		//OEDF
 	}
 	return res;
 }
+
+void key() {
+    string Key = "";
+    Key = random_generator(enCharType::CapitalLetter) + "-";
+    Key = Key + random_generator(enCharType::CapitalLetter) + "-";
+    Key = Key + random_generator(enCharType::CapitalLetter) + "-";
+    Key = Key + random_generator(enCharType::CapitalLetter);
+    cout << Key;
+}
+
 void generated_keys(int n) {
 	int count = 0;
 	
 	for (int i = 1; i <= n; i++) {
 		count++;
 		cout <<"Key " << "[ " << count << " ]: ";
-		
-		cout << random_generator()<<"-" << random_generator()<<"-"<< random_generator()<<"-"<< random_generator() << endl;
+        key();
+        cout << endl;
 		
 	}
 }
