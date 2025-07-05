bbf253bde1aaa4c3482c152c35cd19fe586614b4
osama mohamoud <wwwhdosama@gmail.com>
2025-07-05 15:43:04 +0300

    Auto commit: 2025-07-05 15:43:04

---
+++ ConsoleApplication27/ConsoleApplication27.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int random(int from, int to) {
+	int random = rand() % (to - from + 1) + from;
+	return random;
+}
+void array_filled_with_random(int arr[100], int& length)
+{
+	cout << "enter length ";
+	cin >> length;
+	for (int i = 0; i < length; i++)
+		arr[i] = random(1, 100);
+}
+void array_output(int arr[100], int length)
+{
+	for (int o = 0; o < length; o++)
+		cout << arr[o] << " ";
+}
+int sum(int arr[100], int length)
+{
+	int sum = 0; 
+	for (int s = 0; s < length; s++)
+		sum += arr[s];
+	return sum;
+}
+double  avg(int arr[100], int length)
+{
+	
+	
+	return ((float)sum(arr,length) / length);
+}
+
+
+int main()
+ {
+
+	srand((unsigned)time(NULL));
+	int arr[100];
+	int length = 0;
+	array_filled_with_random(arr, length);
+	cout << endl;
+	array_output(arr, length);
+	cout << endl;
+	cout << "the avg is " << avg(arr, length) << endl;
+	
+}
