db1fda6847bb7055a2edae0808bb6f982aa28cc3
osama mohamoud <wwwhdosama@gmail.com>
2025-07-10 13:59:22 +0300

    Auto commit: 2025-07-10 13:59:21

--- ConsoleApplication40/ConsoleApplication40.cpp
+++ ConsoleApplication40/ConsoleApplication40.cpp
@@ -1,10 +1,10 @@
-// use fill array function to  fill only distinct nums to array 2
-#include <iostream>
 
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-void hardcoded_array_(int arr[10], int length) {
-	//array defined by user 
+void hard_coded_filled_array(int arr[10], int &length) {
 	arr[0] = 10;
 	arr[1] = 10;
 	arr[2] = 10;
@@ -20,64 +20,56 @@ void hardcoded_array_(int arr[10], int length) {
 	arr[9] = 90;
 
 }
-int check_is_number_exist(int number_to_check, int arr[10], int length) {
-	for (int k = 0; k < length; k++) {
-		if (arr[k] == number_to_check) return k; // return index
+
+int check_the_availabilty_of_number_in_array(int number, int ANYarray[10] , int length) {
+	for (int c = 0; c < length; c++)
+		if (ANYarray[c] == number) return c;
+
+return -1;
+}
+
+bool Number_is_available(int number, int ANYarray[10], int length) {
+	return check_the_availabilty_of_number_in_array(number, ANYarray, length) != -1;
+	//if it isn't equal to -1 then it is true , true means that the number is exist in array
+}
+
+void fill_array2(int number , int arr2[10] , int &length2) {
+	// first index is 0 
+	length2++; // 0+1 = 1
+	arr2[length2-1] = number;  //1-1=0
+
+}
+
+void check_then_fill_distinct(int arr[10],int length , int arr2[10] ,int &length2) {
+	// walk through array1 , if the number that from Array1 isn't existing in array two then fill
+	// array2 with this number 
+	for (int f = 0; f < length; f++) {
+		if (!Number_is_available(arr[f], arr2, length))
+			fill_array2(arr[f], arr2, length2);
+
 	}
-		return -1;
-}
-bool is_number_exist_really(int number_to_check, int arr[10], int length) {
-	return check_is_number_exist(number_to_check, arr, length) != -1; // then it is true 
-}
-void fill_array2_elements(int number, int& length2, int arr2[10]) {
-	length2++;
-	arr2[length2 - 1] = number;
 
 }
-void fill_array_with_distict_values(int arr[10], int arr2[10], int length, int& length2) {
 
-	int number_before = 0;
-	for (int d = 0; d < length; d++) {
-//		if (arr[d] == number_before) continue; // is 10 ==0 (NO)  
-//// is 10 ==10 (yes) the it will skip this number and add+1 to d so will continue loop 
-//// 		   // is 20 ==20 (yes) the it will skip this number and add+1 to d so will continue loop 
-//		// is 20 == 10 (NO)
-//		fill_array2_elements(arr[d], length2, arr2); // then number=10
-//		                                                     // the number =20  
-//		number_before = arr[d]; // number_before = 10
-//		                        //  number_before = 20
-		number_before = arr[d];
-		if (!is_number_exist_really(number_before, arr2, length) ) {
-			fill_array2_elements(arr[d], length2, arr2);
-		}
-	}
-	 
-
-
-
-}
-void output_any_array(int Anyarr[100], int length) {
-	cout << "\n";
+void output(int ANYarray[10], int length) {
 	for (int o = 0; o < length; o++)
-		cout << Anyarr[o] << " ";
+		cout << ANYarray[o] << " ";
 }
 
 int main() {
-	int length =10;
-	int length2 = 0;
+	int length = 10;
+	int length2 = 0; 
 	int arr[10], arr2[10];
-	hardcoded_array_(arr, length);
-	
-	cout << '\n';
-	cout << "original array: ";
-	output_any_array(arr, length);
-
-
-	fill_array_with_distict_values(arr, arr2, length, length2);
+	// for the original array 
+	hard_coded_filled_array(arr, length);
+	cout << "the original array are: ";
+	output(arr, length);
 	cout << endl;
-	cout << "\n";
-	cout << "only distinct nums: ";
-	output_any_array(arr2, length2);
-	cout << "\n";
+	cout << endl;
+	//for the distinct array 
+	check_then_fill_distinct(arr, length, arr2, length2);
+	cout << "the distinct numbers are: ";
+	output(arr2, length2);
+	cout << endl;
 
 }
