9f19b8c0fc47c9e9456186bb9e7fbccc17e2202c
osama mohamoud <wwwhdosama@gmail.com>
2025-07-11 16:13:23 +0300

    Auto commit: 2025-07-11 16:13:23

---
+++ ConsoleApplication49/ConsoleApplication49.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+float input_number() {
+	cout << "enter number to floor: ";
+	float  n;
+	cin >> n;
+	return n;
+}
+double ceil_function(float n) {
+	// 10.7 --> 11
+
+	int INT_part_number = n; //10 
+	float FLOAT_part = n; //10.7
+	float fraction = FLOAT_part - INT_part_number; //0.7
+
+	
+	if (n > 0.0) {  //postive 
+		if (fraction == 0.0 ) return INT_part_number;
+		else return  (FLOAT_part - fraction) + 1;
+	}
+	if (0.0 > n) { // negative 
+
+		if ( fraction == -0.0) return INT_part_number;
+		else return (abs(FLOAT_part - fraction) )*-1;
+	}
+
+
+	if (FLOAT_part == 0.0)return 0;
+
+}
+int main() {
+	float  input = input_number();
+	cout << "\nmy ceil function " << ceil_function(input);
+	cout << "\nCPP floor: " << ceil(input) << endl;
+
+}
